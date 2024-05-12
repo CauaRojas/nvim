dd965fae8617d82ecda063ab7b910c7342ff0905
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-05-12 10:44:40 -0300

    added terminal

--- lua/cauarojas/packer.lua
+++ lua/cauarojas/packer.lua
@@ -4,49 +4,56 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	use("navarasu/onedark.nvim")
-	use({ "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } })
-	use("mbbill/undotree")
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		requires = {
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "neovim/nvim-lspconfig" },
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-		},
-	})
-	use({ "nvimtools/none-ls.nvim" })
-	use({ "nvim-lua/plenary.nvim" })
-	use({ "github/copilot.vim" })
-	use({ "CopilotC-Nvim/CopilotChat.nvim" })
-	use("nvim-tree/nvim-web-devicons")
-	use("nvim-tree/nvim-tree.lua")
-	use({
-		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
-		config = function()
-			require("dashboard").setup({
-				-- config
-			})
-		end,
-		requires = { "nvim-tree/nvim-web-devicons" },
-	})
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
-	})
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        -- or                            , branch = '0.1.x',
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
+    use("navarasu/onedark.nvim")
+    use({ "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } })
+    use("mbbill/undotree")
+    use({
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        requires = {
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
+            { "neovim/nvim-lspconfig" },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "L3MON4D3/LuaSnip" },
+        },
+    })
+    use({ "nvimtools/none-ls.nvim" })
+    use({ "nvim-lua/plenary.nvim" })
+    use({ "github/copilot.vim" })
+    use({ "CopilotC-Nvim/CopilotChat.nvim" })
+    use("nvim-tree/nvim-web-devicons")
+    use("nvim-tree/nvim-tree.lua")
+    use({
+        "nvimdev/dashboard-nvim",
+        event = "VimEnter",
+        config = function()
+            require("dashboard").setup({
+                -- config
+            })
+        end,
+        requires = { "nvim-tree/nvim-web-devicons" },
+    })
+    use({
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
+    })
     use("norcalli/nvim-colorizer.lua")
+    use({
+        "akinsho/toggleterm.nvim",
+        tag = "*",
+        config = function()
+            require("toggleterm").setup()
+        end,
+    })
 end)
--- lua/cauarojas/remap.lua
+++ lua/cauarojas/remap.lua
@@ -1,26 +1,60 @@
+-- Leader key = space
 vim.g.mapleader = " "
+
+-- Project view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+
+-- Move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Move lines and keep cursor position
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- Paste without yanking
 vim.keymap.set("x", "<leader>p", '"_dP')
 
+-- copy to clipboard
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
+-- delete without yanking
 vim.keymap.set("n", "<leader>d", '"_d')
 vim.keymap.set("v", "<leader>d", '"_d')
 
+-- LSP formatting
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format()
 end)
+
+-- format range
 vim.keymap.set("v", "<leader>f", function()
 	vim.lsp.buf.range_format()
 end)
+
+-- nvim-tree toggle
 vim.keymap.set("n", "<leader>tr", vim.cmd.NvimTreeToggle)
 
+-- find and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+
+local terminal = require("toggleterm.terminal").Terminal
+local horizontal_poweshell = terminal:new({cmd = "powershell", hidden = true, direction = "horizontal", size = 10})
+
+function _G.toggle_horizontal_poweshell()
+    horizontal_poweshell:toggle()
+end
+
+-- Open new vertical terminal window
+vim.keymap.set("n", "<leader>tv", function ()
+    vim.cmd("ToggleTerm direction=vertical size=40")
+end)
+
+-- Open new horizontal terminal window
+vim.keymap.set("n", "<leader>th", "<cmd>lua toggle_horizontal_poweshell()<CR>")
+
+
