b0dfcbb27444bb831a75fbcc17ca15984720970f
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-05-25 10:28:48 -0300

    added keymaps for terminal

--- lua/cauarojas/remap.lua
+++ lua/cauarojas/remap.lua
@@ -4,7 +4,6 @@ vim.g.mapleader = " "
 -- Project view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-
 -- Move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -27,12 +26,12 @@ vim.keymap.set("v", "<leader>d", '"_d')
 
 -- LSP formatting
 vim.keymap.set("n", "<leader>f", function()
-	vim.lsp.buf.format()
+    vim.lsp.buf.format()
 end)
 
 -- format range
 vim.keymap.set("v", "<leader>f", function()
-	vim.lsp.buf.range_format()
+    vim.lsp.buf.range_format()
 end)
 
 -- nvim-tree toggle
@@ -41,20 +40,26 @@ vim.keymap.set("n", "<leader>tr", vim.cmd.NvimTreeToggle)
 -- find and replace
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-
 local terminal = require("toggleterm.terminal").Terminal
-local horizontal_poweshell = terminal:new({cmd = "powershell", hidden = true, direction = "horizontal", size = 10})
+local horizontal_poweshell = terminal:new({ cmd = "powershell", hidden = true, direction = "horizontal", size = 10 })
 
 function _G.toggle_horizontal_poweshell()
     horizontal_poweshell:toggle()
 end
 
 -- Open new vertical terminal window
-vim.keymap.set("n", "<leader>tv", function ()
+vim.keymap.set("n", "<leader>tv", function()
     vim.cmd("ToggleTerm direction=vertical size=40")
 end)
 
 -- Open new horizontal terminal window
 vim.keymap.set("n", "<leader>th", "<cmd>lua toggle_horizontal_poweshell()<CR>")
 
-
+local opts = { buffer = 0 }
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
