9f9a4d5f4d3bfe306bf854c3368e271703fe513a
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2024-11-28 10:31:18 -0300

    update config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,28 +1,43 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-  }),
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    }),
 })
 
-
-lsp.on_attach(
-function(client, bufnr)
- lsp.default_keymaps({buffer = bufnr})
-end
-)
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+end)
+vim.api.nvim_create_autocmd("CursorHold", {
+    buffer = bufnr,
+    callback = function()
+        local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = "rounded",
+            source = "always",
+            prefix = " ",
+            scope = "cursor",
+        }
+        vim.diagnostic.open_float(nil, opts)
     end,
-  },
 })
-
-
+require("mason").setup({})
+require("mason-lspconfig").setup({
+    ensure_installed = {},
+    handlers = {
+        function(server_name)
+            require("lspconfig")[server_name].setup({})
+        end,
+    },
+})
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+    group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+    callback = function()
+        vim.diagnostic.open_float(nil, { focus = false })
+    end,
+})
